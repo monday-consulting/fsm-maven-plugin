abebf9e824c250b08ad745d93ae9ac6b3e73a6ce
Oliver Lehmann-Korn <oliver.lehmann-korn@monday-consulting.com>
2025-09-25 13:16:54 +0200

    PrepareMojo: Omit the legacy module descriptor (module.xml)
    Changes the default behavior of the PrepareMojo: only the isolated descriptor `module-isolated.xml` will be written, and the legacy descriptor `module.xml` is omitted. FirstSpirit’s legacy mode has been discontinued, so the file is no longer needed.
    
    The creation of the legacy descriptor can be enabled via configuration if needed:
    `<legacyModuleDescriptor>true</legacyModuleDescriptor>`
    
    FC-2762

--- src/main/java/com/monday_consulting/maven/plugins/fsm/xml/ModuleXmlWriter.java
+++ src/main/java/com/monday_consulting/maven/plugins/fsm/xml/ModuleXmlWriter.java
@@ -22,27 +22,33 @@ public class ModuleXmlWriter {
     }
 
     /**
-     * Write a DOM-Tree to a target directory.
-     * This will create {@code ./META-INF/module.xml} and {@code ./META-INF/module-isolated.xml}
+     * Write a DOM tree into the given target directory.
+     * <p>
+     * This method always writes {@code ./META-INF/module-isolated.xml}. If {@code legacyModule} is
+     * {@code true}, it also writes a legacy descriptor at {@code ./META-INF/module.xml} (a copy of
+     * {@code module-isolated.xml}).
      *
-     * @param target target directory to write to
-     * @param dom    dom to write to file.
-     * @throws IOException in case of unexpected writer exceptions.
+     * @param target        the target directory to write to; it will be created along with {@code META-INF} if missing
+     * @param dom           the DOM to serialize into the descriptor file(s)
+     * @param legacyModule  when {@code true}, additionally create {@code module.xml} alongside {@code module-isolated.xml}
+     * @throws IOException  in case of unexpected writer exceptions
      */
-    public void writeDomToTarget(final Path target, final Xpp3Dom dom) throws IOException {
+    public void writeDomToTarget(final Path target, final Xpp3Dom dom, final boolean legacyModule) throws IOException {
         log.debug("Write TargetXml-File");
 
         final Path metaInf = target.resolve("META-INF");
         // ensure that the target directory exists
         Files.createDirectories(metaInf);
 
-        final Path moduleXml = metaInf.resolve("module.xml");
-        log.info("Writing module descriptor: " + moduleXml);
-        writeFile(moduleXml.toFile(), dom);
-
         final Path moduleIsolatedXml = metaInf.resolve("module-isolated.xml");
         log.info("Writing isolated module descriptor: " + moduleIsolatedXml);
-        Files.copy(moduleXml, moduleIsolatedXml, StandardCopyOption.REPLACE_EXISTING);
+        writeFile(moduleIsolatedXml.toFile(), dom);
+
+        if (legacyModule) {
+            final Path moduleXml = metaInf.resolve("module.xml");
+            log.info("Writing module descriptor: " + moduleXml);
+            Files.copy(moduleIsolatedXml, moduleXml, StandardCopyOption.REPLACE_EXISTING);
+        }
 
         log.debug("Dependencies written to Module-XML:\n\t" + dom);
     }
